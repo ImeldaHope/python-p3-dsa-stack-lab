32757c83650b811ec8b892057552091842d1264b
Imelda Hope <imeldalarusso@gmail.com>
2024-09-16 06:56:32 +0300

    implements a stack

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,38 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        if not self.full():
+            return self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
+        return self.items[0]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return len(self.items) - 1 - self.items.index(target)
+        
+        return -1
